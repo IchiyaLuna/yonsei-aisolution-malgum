78c9ead013a7548cd80bc5e581af422bd30c9871
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2024-10-30 05:21:56 +0900

    :truck: app modules moved to own directory
    src -> src/app

--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessagesModule } from '@/message/message.module';
 
 @Module({
-  imports: [],
+  imports: [MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
